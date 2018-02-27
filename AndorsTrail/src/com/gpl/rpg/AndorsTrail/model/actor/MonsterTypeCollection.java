7d2134205c73e0304aa294300bab1a81b49f7011
Zukero <kpochat@gmail.com>
2018-02-27 14:29:39 +0100

    SpawnArea's spawn group can refer to a specific monster type, even if the spawn group doesn't exist and that this monster type belongs to a specific spawngroup (like in ATCS).

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/model/actor/MonsterTypeCollection.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/model/actor/MonsterTypeCollection.java
@@ -24,7 +24,11 @@ public final class MonsterTypeCollection {
 		for (MonsterType t : monsterTypesById.values()) {
 			if (t.spawnGroup.equalsIgnoreCase(spawnGroup)) result.add(t);
 		}
-
+		//If the spawnGroup is empty, it should be a direct reference to a MonsterType's id.
+		if (result.isEmpty()) {
+			MonsterType t = monsterTypesById.get(spawnGroup);
+			if (t != null) result.add(t);
+		}
 		return result;
 	}
 
