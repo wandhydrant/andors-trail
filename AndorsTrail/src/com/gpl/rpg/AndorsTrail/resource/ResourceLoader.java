1e3f3310c26c2f6dbf22958b28b8dae0def44f54
Nut <nut.andor@o2online.de>
2018-07-20 16:52:16 +0200

    new graphic monsters_maksiu1, and an author list entry

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/resource/ResourceLoader.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/resource/ResourceLoader.java
@@ -334,6 +334,7 @@ public final class ResourceLoader {
 		loader.prepareTileset(R.drawable.monsters_zombie1, "monsters_zombie1", sz1x1, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.monsters_zombie2, "monsters_zombie2", sz1x1, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.monsters_guynmart, "monsters_guynmart", new Size(8, 2), sz1x1, mTileSize);
+		loader.prepareTileset(R.drawable.monsters_maksiu1, "monsters_maksiu1", new Size(4, 4), sz1x1, mTileSize);		
 		/*INSERT_NPCS_TILESETS_HERE*/
 		
 		loader.prepareTileset(R.drawable.map_bed_1, "map_bed_1", mapTileSize, sz1x1, mTileSize);
