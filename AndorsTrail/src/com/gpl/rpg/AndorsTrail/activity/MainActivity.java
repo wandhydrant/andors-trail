c8f6e3c3f588157c9aa3bb8d1caa5b3d4bfd4daf
Javier Serrano Polo <javier@jasp.net>
2020-05-23 16:30:02 +0200

    Add Travis CI support

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/activity/MainActivity.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/activity/MainActivity.java
@@ -305,7 +305,8 @@ public final class MainActivity
 			t = Toast.makeText(this, msg, duration);
 			lastToast = new WeakReference<Toast>(t);
 		} else {
-			if (Build.VERSION.SDK_INT  >= Build.VERSION_CODES.P && t.getView().isShown()) {
+//			if (Build.VERSION.SDK_INT  >= Build.VERSION_CODES.P && t.getView().isShown()) {
+			if (Build.VERSION.SDK_INT  >= 28 && t.getView().isShown()) {
 				t.cancel();
 			}
 			t.setText(msg);
