592c676e98bc6bb6b92eab2450191602a26c167d
Nathan Watson <at@atsymboldot.com>
2020-11-24 21:05:28 -0800

    add passive achievement check when sleeping/respawning

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/controller/Constants.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/controller/Constants.java
@@ -48,6 +48,7 @@ public final class Constants {
 	public static final String FILENAME_SAVEGAME_FILENAME_PREFIX = "savegame";
 	public static final String PLACEHOLDER_PLAYERNAME = "$playername";
 	public static final String CHEAT_DETECTION_FOLDER = "dEAGyGE3YojqXjI3x4x7";
+	public static final String PASSIVE_ACHIEVEMENT_CHECK_PHRASE = "passive_achievement_check";
 
 	public static final Random rnd = new Random();
 	public static int rollValue(final ConstRange r) { return rollValue(r.max, r.current); }
