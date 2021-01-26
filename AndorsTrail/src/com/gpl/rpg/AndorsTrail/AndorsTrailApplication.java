aa946bb04738b70a8366eb4e731983d511974c22
Nut.andor <nut.andor@posteo.de>
2021-01-26 15:22:47 +0100

    fix merge conflict

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/AndorsTrailApplication.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/AndorsTrailApplication.java
@@ -30,13 +30,8 @@ public final class AndorsTrailApplication extends Application {
 	public static final boolean DEVELOPMENT_DEBUGMESSAGES = false;
 	public static final boolean DEVELOPMENT_INCOMPATIBLE_SAVEGAMES = DEVELOPMENT_DEBUGRESOURCES || DEVELOPMENT_DEBUGBUTTONS || DEVELOPMENT_FASTSPEED;
 	public static final int DEVELOPMENT_INCOMPATIBLE_SAVEGAME_VERSION = 999;
-<<<<<<< HEAD
-	public static final int CURRENT_VERSION = DEVELOPMENT_INCOMPATIBLE_SAVEGAMES ? DEVELOPMENT_INCOMPATIBLE_SAVEGAME_VERSION : 56;
-	public static final String CURRENT_VERSION_DISPLAY = "0.7.12.1";
-=======
 	public static final int CURRENT_VERSION = DEVELOPMENT_INCOMPATIBLE_SAVEGAMES ? DEVELOPMENT_INCOMPATIBLE_SAVEGAME_VERSION : 58;
 	public static final String CURRENT_VERSION_DISPLAY = "0.7.13";
->>>>>>> Andors-Trail/test_fungi
 	public static final boolean IS_RELEASE_VERSION = !CURRENT_VERSION_DISPLAY.matches(".*[a-d].*");
 
 	private final AndorsTrailPreferences preferences = new AndorsTrailPreferences();
